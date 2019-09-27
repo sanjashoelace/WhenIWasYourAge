6c2b4d6d4fc7a3c6ea5d66576af6a0e1f1b76191
Sanja Smrzlic <sanja@shoelace.com>
2019-09-27 16:06:35 -0400

    Updates

---
+++ precache-manifest.1f5ad2a12632df84cbfb947a09066a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0815c606109f0008a0cefb4fa37c62",
+    "url": "/WhenIWasYourAge/index.html"
+  },
+  {
+    "revision": "7310860409650a969783",
+    "url": "/WhenIWasYourAge/static/css/main.a5751bc2.chunk.css"
+  },
+  {
+    "revision": "91eaba3269fb7a988dc2",
+    "url": "/WhenIWasYourAge/static/js/2.0e7456fb.chunk.js"
+  },
+  {
+    "revision": "7310860409650a969783",
+    "url": "/WhenIWasYourAge/static/js/main.9445e693.chunk.js"
+  },
+  {
+    "revision": "73b2732375d47a765771",
+    "url": "/WhenIWasYourAge/static/js/runtime-main.a88d4c9c.js"
+  }
+]);
