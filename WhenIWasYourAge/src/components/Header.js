2715bca63b58674210a5a4083f92746a81247b7c
Sanja Smrzlic <sanja@shoelace.com>
2019-09-27 16:00:11 -0400

    naming

---
+++ WhenIWasYourAge/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            When I was your age . . .
+        </header>
+    )
+}
+
+export default Header;
---
+++ WhenIWasYourAge/src/constants/api_keys.js
@@ -0,0 +1,4 @@
+export const LAST_FM_API_KEY= '621d4bf04d40033003b4853c39ce536c';
+export const MOVIE_DB_API_KEY = '7eecbc420d0abb454bb030dcc47e157f';
+
+export default { LAST_FM_API_KEY, MOVIE_DB_API_KEY }
